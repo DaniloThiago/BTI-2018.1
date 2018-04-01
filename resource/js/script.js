e3121ea6764314301d2aea97961d6ff75ff60fb2
DaniloOliveira <danilo.t.a.oliveira@hotmail.com>
2018-04-01 13:07:47 -0300

    Mat Elem

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,5 +1,6 @@
 $('#menu a').click(function (e) {
     let name = $(this).data('name');
+    $(this).blur();
     if (!name) return;
     $('.materias').removeClass('active');
     $('#' + name).addClass('active');
