25efc9446ce99069545d0b3258f3e97b0879dfac
DaniloOliveira <danilo.t.a.oliveira@hotmail.com>
2018-04-01 14:55:38 -0300

    Mat Elem - união

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,7 +1,7 @@
 $('#menu a').click(function (e) {
-    let name = $(this).data('name');
-    $(this).blur();
-    if (!name) return;
-    $('.materias').removeClass('active');
-    $('#' + name).addClass('active');
+	let name = $(this).data('name');
+	$(this).blur();
+	if (!name) return;
+	$('.materias').removeClass('active');
+	$('#' + name).addClass('active');
 });
